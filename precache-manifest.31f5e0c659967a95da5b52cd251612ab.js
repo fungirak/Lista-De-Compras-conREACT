8a1ecc4e94291bb206544ceb1fc1ae45614a1c0c
Gabriel Lazzarini <gabilazzarini88@outlook.com>
2021-08-12 19:18:52 -0300

    Updates

---
+++ precache-manifest.31f5e0c659967a95da5b52cd251612ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4377f9cbf1b738760f39e2fe91c8e1b2",
+    "url": "/Lista-De-Compras-conREACT/index.html"
+  },
+  {
+    "revision": "0991911098102b0408aa",
+    "url": "/Lista-De-Compras-conREACT/static/css/2.86fbe410.chunk.css"
+  },
+  {
+    "revision": "eeaaa7a40577f433ca32",
+    "url": "/Lista-De-Compras-conREACT/static/css/main.2561c073.chunk.css"
+  },
+  {
+    "revision": "0991911098102b0408aa",
+    "url": "/Lista-De-Compras-conREACT/static/js/2.221695e4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Lista-De-Compras-conREACT/static/js/2.221695e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeaaa7a40577f433ca32",
+    "url": "/Lista-De-Compras-conREACT/static/js/main.e4750dd3.chunk.js"
+  },
+  {
+    "revision": "0190b99e7a0828febfff",
+    "url": "/Lista-De-Compras-conREACT/static/js/runtime-main.99bc015f.js"
+  }
+]);
